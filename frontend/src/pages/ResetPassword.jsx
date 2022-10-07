db52adacfb78f90e2f4a86d1c3b2ab250ca99afd
LebronBrianSinza <lebronbrian23@gmail.com>
2022-10-07 11:19:11 -0400

    changed  developer to dev

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -91,7 +91,6 @@ function ResetPassword () {
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                             id="password" type="password" placeholder="*********" onChange={onChange} value={password} name='password'/>
-
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirm-password">
@@ -116,7 +115,6 @@ function ResetPassword () {
             </div>
         </div>
     </>
-
 }
 
 export default ResetPassword
