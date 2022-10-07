7e7a988499625ae0841546de84a7e59879c60e2a
LebronBrianSinza <lebronbrian23@gmail.com>
2022-10-07 11:57:23 -0400

    text wrap

--- frontend/src/components/ProfileSidebar.jsx
+++ frontend/src/components/ProfileSidebar.jsx
@@ -44,10 +44,8 @@ function ProfileSidebar() {
                             <span className="username">{userData.email}</span>
                             <span className="username">{userData.phone}</span>
                             <div className="d-flex flex-row justify-content-center align-items-center flex-wrap">
-                                <div className=" text-wrap">
                                     <span className="profile-link">{generate_profile_link}</span>
-                                    <span><button onClick={copy} disabled={!generate_profile_link}> <FaRegClone className='p-2'/></button></span>
-                                </div>
+                                    <span className='p-2'><button onClick={copy} disabled={!generate_profile_link}> <FaRegClone/></button></span>
                             </div>
                             <div className="bio mt-3">
                                 <span>{userData.bio} </span>
