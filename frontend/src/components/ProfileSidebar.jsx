6ce3b62a372dfc9ed3cd8d4f298edd27198fc605
LebronBrianSinza <lebronbrian23@gmail.com>
2022-10-07 11:48:05 -0400

    text break

--- frontend/src/components/ProfileSidebar.jsx
+++ frontend/src/components/ProfileSidebar.jsx
@@ -43,8 +43,8 @@ function ProfileSidebar() {
                             <span className="username">{userData.username}</span>
                             <span className="username">{userData.email}</span>
                             <span className="username">{userData.phone}</span>
-                            <div className="d-flex flex-row justify-content-center align-items-center gap-2">
-                                <span className="profile-link text-break">{generate_profile_link}</span>
+                            <div className="d-flex flex-row justify-content-center align-items-center flex-wrap">
+                                <span className="profile-link text-nowrap">{generate_profile_link}</span>
                                 <span><button onClick={copy} disabled={!generate_profile_link}><FaRegClone/></button></span>
                             </div>
                             <div className="bio mt-3">
