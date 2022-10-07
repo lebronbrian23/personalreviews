31f417676a447fd1dc94f0a93b474c554af85c18
LebronBrianSinza <lebronbrian23@gmail.com>
2022-10-07 01:35:10 -0400

    changed  developer to dev

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -94,12 +94,12 @@ function ResetPassword () {
 
                     </div>
                     <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirm_password">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirm-password">
                             Confirm Password <sup className='text-red-700 text-lg'>*</sup>
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password" type="password" placeholder="*********" onChange={onChange} value={confirm_password} name='confirm_password'/>
+                            id="confirm-password" type="password" placeholder="*********" onChange={onChange} value={confirm_password} name='confirm_password'/>
 
                     </div>
                     <div className="flex items-center justify-between">
