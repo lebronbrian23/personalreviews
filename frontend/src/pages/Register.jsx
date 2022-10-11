7699e25fca797e7da81efdfec55cbacf51892e41
LebronBrianSinza <lebronbrian23@gmail.com>
2022-10-11 10:10:33 -0400

    fixed issue on registering user email

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -103,7 +103,7 @@ function Register () {
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            type='text' name='name' id='email' value={email} placeholder='email@example.com' onChange={onChange}/>
+                            type='text' name='email' id='email' value={email} placeholder='email@example.com' onChange={onChange}/>
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
